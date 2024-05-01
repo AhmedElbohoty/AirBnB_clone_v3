00b33332b9c0f55b10508d13f82ac769bfa4f6e7
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-05-01 17:32:30 +0300

    Fix simple issue

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,13 +6,13 @@ from models import storage
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns the status of the API """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """Retrieves the number of each object by type"""
     classes = {
